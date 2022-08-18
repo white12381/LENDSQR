4da38e42751578f7079abcd656fb87bc76745ba2
usman <>
2022-08-18 16:38:49 +0100

    added port to knex.js

--- Controller/knex.js
+++ Controller/knex.js
@@ -2,7 +2,7 @@ const knex = require("knex")({
   client: "mysql",
   connection: {
     host: "us-cdbr-east-06.cleardb.net",
-    // port: 3306,
+    port: 3306,
     user: "b36312e1b6e987",
     database: "heroku_4c19371dc74f806",
       password : 'aad7a27f'
