bc66070dd4c32c43a61d7c70b8f97d9b0d1d562d
usman <>
2022-08-18 17:51:08 +0100

    show port value

--- start.js
+++ start.js
@@ -1,4 +1,4 @@
 const server = require("./Controller/index");
 const port = 3000 || process.env.Port
 server.listen(port);
-console.log("port running on 3000");
+console.log(`port running on ${port}`);
